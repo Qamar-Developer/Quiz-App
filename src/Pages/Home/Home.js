b9d8a604c793926d42723c1e5e0503ce160577ce
Qamar Shahzad <112967940+Qamar-Developer@users.noreply.github.com>
2022-09-06 22:14:19 +0500

    Add files via upload

---
+++ src/Pages/Home/Home.js
@@ -0,0 +1,84 @@
+import { Button, MenuItem, TextField } from "@material-ui/core";
+import "./Home.css";
+import Categories from '../../Data/Categories'
+import { useState } from "react";
+import {useNavigate } from "react-router";
+import ErrorMessage from "../../components/ErrorMessage/ErrorMessage";
+const Home = ({name, setName , fetchQuestions}) => {
+    const [category, setCategory] = useState("");
+    const [difficulty, setDifficulty] = useState("");
+   const [error, setError] = useState(false);
+
+    const navigate= useNavigate();
+
+    const handleSubmit=()=> {
+        if(!category || !difficulty || !name) {
+            setError(true);
+            return;
+        }
+        else {
+            setError(false);
+            fetchQuestions(category, difficulty);
+            navigate("/quiz");
+          }
+    };
+
+    return (
+    <div className="content">
+      <div className="settings">
+        <span style={{ fontSize: 30 }}>Quiz Settings</span>
+
+        <div className="setting_select">
+        {error && <ErrorMessage>Please Fill all the feilds</ErrorMessage>}
+        <TextField
+         style={{marginBottom: 25}}
+         label="Enter Your Name" 
+         variant="outlined" 
+         onChange={(e) => setName(e.target.value)}
+         />
+         <TextField 
+             select label="Select Category" 
+             variant = "outlined" 
+             style={{marginBottom: 30}}
+             onChange={(e) => setCategory(e.target.value)}
+             value={category}
+         >
+         {Categories.map((cat) => (
+              <MenuItem key={cat.category} value={cat.value}>
+                {cat.category}
+              </MenuItem>
+             ))}
+         </TextField>
+
+         <TextField
+            select
+            label="Select Difficulty"
+            value={difficulty}
+            onChange={(e) => setDifficulty(e.target.value)}
+            variant="outlined"
+            style={{ marginBottom: 30 }}
+          >
+            <MenuItem key="Easy" value="easy">
+              Easy
+            </MenuItem>
+            <MenuItem key="Medium" value="medium">
+              Medium
+            </MenuItem>
+            <MenuItem key="Hard" value="hard">
+              Hard
+            </MenuItem>
+            </TextField>
+        
+         <Button variant="contained" color="primary" size="large"
+         onClick={handleSubmit}
+         >
+         Start Quiz
+         </Button>
+        </div>
+        </div>
+      <img src="/quiz.svg" className="banner" alt="quiz app"  />
+    </div>
+  );
+
+};
+export default Home;
---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,13 @@
+const Footer = () => {
+    return (
+        <div style={{textAlign: 'center' , marginBottom: 10}}>
+            Made by: {"Qamar Shahzad "}
+            <a
+            href = "https://www.youtube.com/qamardesigner2016"
+            style = {{cursor: "pointer"}}>
+            
+            </a>
+        </div>
+    )
+}
+export default Footer
